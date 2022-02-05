3079895f1966e6742ebedca7c22017f0fda9cf44
Ruben Aleman <aleman@itq.de>
2022-02-05 10:50:44 +0100

    Initial commit from local

--- do_stuff.py
+++ do_stuff.py
@@ -3,8 +3,6 @@ import pandas as pd
 from IPython.display import display
 import os
 
-print('This is where the owl magic is happening')
-print('Count me baby one more time')
 
 # requires installation  pandas, openpyxl, IPython (for instance via 
 # pip install pandas openpyxl IPython
@@ -26,44 +24,45 @@ for files in os.listdir('xlsx'):
 
 print(listOfFiles)
 
-def logic(index): # returns true for even numbers, false for uneven numbers
+def logic(index): # returns true for even numbers, false for odd numbers
     if index % 2 == 0:
        return True
     return False
 
 dataframe =pd.DataFrame() #Empty object, the loop below adds the Columns and Rows to it
 
-os.chdir('csv/') # Changes working directory to the csv subfolder
+## Comment this in if you want to parse .csv files
+#os.chdir('csv/') # Changes working directory to the csv subfolder
 
-for x in listOfFiles[0]: #loops through the list of files in the csv and xlsx directory, and appends each to dataframe
-    i = pd.read_csv(
-        x,
-        header=6,
-        usecols = [0, 7, 10], # column number, first is 0. 0 = Title, 7 = Online_ISSN, 10 = Reporting_Period_Total
-        skiprows= lambda x: logic(x), # calls the logic function declared above, skips uneven row numbers (the rows with total requests)
-        )
-    #display(i)
-    dataframe = dataframe.append(i, ignore_index=True) # appends adds each csv content to end of the dataframe, ignore_index is there so he doesnt also print the original row number
+# for x in listOfFiles[0]: #loops through the list of files in the csv and xlsx directory, and appends each to dataframe
+#     i = pd.read_csv(
+#         x,
+#         header=6,
+#         usecols = [0, 1, 7, 10], # column number, first is 0. 0 = Title, 7 = Online_ISSN, 10 = Reporting_Period_Total
+#         #skiprows= lambda x: logic(x), # calls the logic function declared above, skips uneven row numbers (the rows with total requests)
+#         )
+#     #display(i)
+#     dataframe = dataframe.append(i, ignore_index=True) # appends adds each csv content to end of the dataframe, ignore_index is there so he doesnt also print the original row number
 
-os.chdir('../xlsx') #Changes working directory to the xlsx subfolder
+os.chdir('xlsx') #Changes working directory to the xlsx subfolder
 
 
-# Returns a DataFrame
-#pd.read_excel("path_to_file.xls", sheet_name="Sheet1")
 
 for x in listOfFiles[1]: #loops through the list of files in the csv and xlsx directory, and appends each to dataframe
     i = pd.read_excel(
         x,
-        header=6,
-        usecols = [0, 7, 10], # column number, first is 0. 0 = Title, 7 = Online_ISSN, 10 = Reporting_Period_Total
-        skiprows= lambda x: logic(x), # calls the logic function declared above, skips uneven row numbers (the rows with total requests)
+        header=13,
+        usecols = [0, 1, 7, 9, 10], # column number, first is 0. 0 = Title, 1 = Publisher, 7 = Online_ISSN, 9 = Metric_Type, 10 = Reporting_Period_Total
+        #skiprows= lambda x: logic(x), # calls the logic function declared above, skips uneven row numbers (the rows with total requests)
         )
-    #display(i)
     dataframe = dataframe.append(i, ignore_index=True) # appends adds each csv content to end of the dataframe, ignore_index is there so he doesnt also print the original row number
 
 
-os.chdir('../')
-display(dataframe) 
+os.chdir('../') # changes working directory to main folder 
+
+display(dataframe) # optional, shows the new table in the console
+dataframe = dataframe.loc[dataframe['Metric_Type'] == "Unique_Item_Requests"]
+display(dataframe) # optional, shows the new table in the console
 dataframe.to_csv("masterlist.csv", index=False) # writes the dataframe to a new file
         
 
