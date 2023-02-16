5f52808b357e0095c2ba45f13e9394f272b1e8b3
Ruben Aleman <ruben.aleman@makandra.de>
2023-02-16 07:46:15 +0100

    add license disclaimer

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+# Counter 4 Counter 5 Parsing Copyright (C) 2023  Ruben Aleman
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
 """Module providing basic os functions"""
 import os
 # import pprint
